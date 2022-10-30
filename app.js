403a03f8341efacb76374f137dd48162da0d269d
deepak vishwakarma <deepakvishwakarma9870@gmail.com>
2022-10-30 18:11:43 +0530

    modified:   app.js modified:   package-lock.json

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+<<<<<<< Updated upstream
 const userRoute = require("./routes/userRoute");
 app.use("/user", userRoute);
+=======
+>>>>>>> Stashed changes
 
 module.exports = app;
